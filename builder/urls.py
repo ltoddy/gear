ae866d87d24f1250dc56d8d2607208c6fa8b39a0
ltoddy <1205446803@qq.com>
2017-10-30 21:05:04 +0800

    url config

--- builder/urls.py
+++ builder/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = ()
+from django.conf.urls import url
+from .view import page
+
+urlpatterns = (
+    url(r'^(?P<name>[\w./-]+)$', page, name='page'),
+    url(r'^$', page, name='homepage'),
+)
